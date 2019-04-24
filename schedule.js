a73ea3b235981b8ac09cd48fbb3586b75b4a283b
huong9x <huongnv@sphinx-software.com>
2019-04-24 09:57:50 +0700

    test booking

--- schedule.js
+++ schedule.js
@@ -1,5 +1,14 @@
 class Schedule {
+
     constructor(policies) {
         this.policies = policies;
+    }
+
+    createSchedule(patient, doctor, falcuty, date) {
+
+    }
+    
+    modifySChedule(schedule, doctor, date) {
+
     }
 }
